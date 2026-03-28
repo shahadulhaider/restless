f7854abce2892547891148bacee3aae17f100669
Shahadul Haider <shahadul.haider@gmail.com>
2026-03-28 16:49:19 +0600

    feat(tui): wire all components, add CLI entry point

--- cmd/restless/main.go
+++ cmd/restless/main.go
@@ -1,3 +1,161 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+	"github.com/shahadulhaider/restless/internal/engine"
+	"github.com/shahadulhaider/restless/internal/importer"
+	"github.com/shahadulhaider/restless/internal/parser"
+	"github.com/shahadulhaider/restless/internal/tui"
+)
+
+var version = "dev"
+
+func main() {
+	if err := rootCmd().Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func rootCmd() *cobra.Command {
+	root := &cobra.Command{
+		Use:   "restless [directory]",
+		Short: "TUI HTTP client using .http files",
+		Args:  cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			dir := "."
+			if len(args) == 1 {
+				dir = args[0]
+			}
+			abs, err := filepath.Abs(dir)
+			if err != nil {
+				return fmt.Errorf("resolving directory: %w", err)
+			}
+			return tui.RunApp(abs)
+		},
+	}
+
+	root.AddCommand(importCmd(), runCmd(), versionCmd())
+	return root
+}
+
+func importCmd() *cobra.Command {
+	var envFile string
+	var outputDir string
+
+	cmd := &cobra.Command{
+		Use:   "import",
+		Short: "Import collections from other tools",
+	}
+
+	postman := &cobra.Command{
+		Use:   "postman <collection.json>",
+		Short: "Import a Postman collection v2.1",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			out := outputDir
+			if out == "" {
+				out = "."
+			}
+			abs, err := filepath.Abs(out)
+			if err != nil {
+				return fmt.Errorf("resolving output dir: %w", err)
+			}
+			if err := importer.ImportPostman(args[0], importer.ImportOptions{OutputDir: abs}); err != nil {
+				return fmt.Errorf("importing collection: %w", err)
+			}
+			if envFile != "" {
+				if err := importer.ImportPostmanEnv(envFile, abs); err != nil {
+					return fmt.Errorf("importing environment: %w", err)
+				}
+			}
+			fmt.Fprintf(os.Stdout, "Imported to %s\n", abs)
+			return nil
+		},
+	}
+	postman.Flags().StringVar(&envFile, "env", "", "Postman environment JSON file")
+	postman.Flags().StringVar(&outputDir, "output", ".", "Output directory")
+
+	cmd.AddCommand(postman)
+	return cmd
+}
+
+func runCmd() *cobra.Command {
+	var envName string
+	var failFast bool
+
+	cmd := &cobra.Command{
+		Use:   "run <file.http>",
+		Short: "Execute all requests in a .http file headlessly",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			filePath := args[0]
+			rootDir := filepath.Dir(filePath)
+
+			reqs, err := parser.ParseFile(filePath)
+			if err != nil {
+				return fmt.Errorf("parsing %s: %w", filePath, err)
+			}
+
+			var envVars map[string]string
+			if envName != "" {
+				envFile, loadErr := parser.LoadEnvironments(rootDir)
+				if loadErr == nil && envFile != nil {
+					envVars, _ = parser.ResolveEnvironment(envFile, envName)
+				}
+			}
+			if envVars == nil {
+				envVars = make(map[string]string)
+			}
+
+			chainCtx := parser.NewChainContext()
+			cookies := engine.NewCookieManager()
+
+			for _, req := range reqs {
+				resolved, _ := parser.ResolveRequest(&req, envVars, chainCtx)
+				loaded, loadErr := parser.LoadFileBody(resolved, rootDir)
+				if loadErr != nil {
+					loaded = resolved
+				}
+
+				jar := cookies.JarForEnv(envName)
+				resp, execErr := engine.ExecuteWithJar(loaded, jar)
+				if execErr != nil {
+					fmt.Fprintf(os.Stderr, "ERROR %s %s: %v\n", loaded.Method, loaded.URL, execErr)
+					if failFast {
+						return execErr
+					}
+					continue
+				}
+
+				if loaded.Name != "" {
+					chainCtx.StoreResponse(loaded.Name, resp)
+				}
+
+				fmt.Fprintf(os.Stdout, "%s %s → %s (%dms)\n",
+					loaded.Method, loaded.URL,
+					resp.Status,
+					resp.Timing.Total.Milliseconds())
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&envName, "env", "", "Environment name")
+	cmd.Flags().BoolVar(&failFast, "fail-fast", false, "Stop on first error")
+	return cmd
+}
+
+func versionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Print version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("restless", version)
+		},
+	}
+}
--- go.mod
+++ go.mod
@@ -21,12 +21,15 @@ require (
 	github.com/clipperhouse/stringish v0.1.1 // indirect
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mattn/go-runewidth v0.0.21 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rbretecher/go-postman-collection v0.9.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/spf13/cobra v1.10.2 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
